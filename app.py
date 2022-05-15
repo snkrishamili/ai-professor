0741eb7eb4f9b8f5bcce4422dbe9fccde56ee138
Praveenath <logeshwaranpraveenath@gmail.com>
2022-05-15 23:22:01 +0530

    Altered app run

--- app.py
+++ app.py
@@ -191,5 +191,5 @@ def help():
     return render_template('default.html')
     # return redirect(url_for('speak'))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=80)
